38a2b4df059b98156b791a159370910654272ba2
redamoon <yuuki60824@gmail.com>
2020-10-31 02:27:01 +0900

    fix

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # mt-plugin-NetlifyBuildHooks
+
+Movable Typeで記事・コンテンツタイプのDataAPIが更新されたタイミングで Netlify Build Hooks を実行するプラグインになります。
+
--- plugins/NetlifyBuildHooks/lib/NetlifyBuildHooks/L10N/ja.pm
+++ plugins/NetlifyBuildHooks/lib/NetlifyBuildHooks/L10N/ja.pm
@@ -5,8 +5,8 @@ use base 'NetlifyBuildHooks::L10N::en_us';
 use vars qw( %Lexicon );
 
 %Lexicon = (
-    'Please input the Netlify Build Hooks ID.' => 'Netlify の Build Hooks IDを入力してください。',
-    'Netlify Build Hooks Plugin' => 'NetlifyのBuild Hooks連携プラグイン',
+    'Please input the Netlify Build Hooks ID.' => 'Netlify Build Hooks IDを入力してください。',
+    'Netlify Build Hooks Plugin' => 'NetlifyBuildHooks',
     '_PLUGIN_DESCRIPTION' => '記事の保存時/再構築時にNetlify Build Hooksを実行する',
     '_PLUGIN_AUTHOR' => 'redamoon',
 );
