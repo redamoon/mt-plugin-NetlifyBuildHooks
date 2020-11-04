ae60290008af96a8120a9e0a448d416c8aea659b
redamoon <yuuki60824@gmail.com>
2020-11-05 00:11:19 +0900

    fix

--- plugins/NetlifyBuildHooks/lib/NetlifyBuildHooks/Callbacks.pm
+++ plugins/NetlifyBuildHooks/lib/NetlifyBuildHooks/Callbacks.pm
@@ -6,7 +6,7 @@ use LWP::UserAgent;
 use HTTP::Request::Common;
 use JSON;
 
-sub post_save_entry {
+sub entry_builds {
     my ($cb, $app, $obj, $org_obj) = @_;
     my $plugin = MT->component('NetlifyBuildHooks');
     require MT::Entry;
@@ -23,6 +23,44 @@ sub post_save_entry {
     $obj->save
         or die $obj->errstr;
     $ua->request($request);
+}
+
+sub page_builds {
+    my ($cb, $app, $obj, $org_obj) = @_;
+    my $plugin = MT->component('NetlifyBuildHooks');
+    require MT::Page;
+    return unless $obj->status == MT::Page::RELEASE();
+
+    my $app_id = $plugin->get_config_value('netlify_build_hooks', 'system');
+    return 1 unless $app_id;
+
+    my $url = NetlifyBuildHooksUrl . $app_id; #webhook url
+    my $request = POST($url, Content_Type => 'application/json', Content => encode_json({}));
+
+    my $ua = LWP::UserAgent->new;
+
+    $obj->save
+        or die $obj->errstr;
+    $ua->request($request);
+}
+
+sub content_data_builds {
+    my ($cb, $app, $obj, $org_obj) = @_;
+    my $plugin = MT->component('NetlifyBuildHooks');
+    require MT::ContentStatus;
+    return unless $obj->status == MT::ContentStatus::RELEASE();
+
+    my $app_id = $plugin->get_config_value('netlify_build_hooks', 'system');
+    return 1 unless $app_id;
+
+    my $url = NetlifyBuildHooksUrl . $app_id; #webhook url
+    my $request = POST($url, Content_Type => 'application/json', Content => encode_json({}));
+
+    my $ua = LWP::UserAgent->new;
+
+    $ua->request($request);
+    $obj->save
+        or die $obj->errstr;
 }
 
 1;
