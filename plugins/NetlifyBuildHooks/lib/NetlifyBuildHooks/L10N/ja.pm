05c55bb554dd33315c1b223b97137564ed0ad529
redamoon <yuuki60824@gmail.com>
2020-11-05 11:27:37 +0900

    add description

--- plugins/NetlifyBuildHooks/lib/NetlifyBuildHooks/L10N/ja.pm
+++ plugins/NetlifyBuildHooks/lib/NetlifyBuildHooks/L10N/ja.pm
@@ -7,7 +7,7 @@ use vars qw( %Lexicon );
 %Lexicon = (
     'Please input the Netlify Build Hooks ID.' => 'Netlify Build Hooks IDを入力してください。',
     'Netlify Build Hooks Plugin' => 'NetlifyBuildHooks',
-    '_PLUGIN_DESCRIPTION' => '記事の保存時/再構築時にNetlify Build Hooksを実行する',
+    '_PLUGIN_DESCRIPTION' => '記事の保存時/再構築時/削除にNetlify Build Hooksを実行する',
     '_PLUGIN_AUTHOR' => 'redamoon',
 );
 
