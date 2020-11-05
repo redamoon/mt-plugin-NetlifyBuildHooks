3c8012c8f8cd5b4f13c16581de7438ea514dfc13
redamoon <yuuki60824@gmail.com>
2020-11-05 13:35:38 +0900

    fix リファクタ

--- plugins/NetlifyBuildHooks/lib/NetlifyBuildHooks/Callbacks.pm
+++ plugins/NetlifyBuildHooks/lib/NetlifyBuildHooks/Callbacks.pm
@@ -6,50 +6,16 @@ use LWP::UserAgent;
 use HTTP::Request::Common;
 use JSON;
 
-sub entry_builds {
+my $plugin = MT->component('NetlifyBuildHooks');
+my $app_id = $plugin->get_config_value('netlify_build_hooks', 'system');
+my $url = NetlifyBuildHooksUrl . $app_id; #webhook url
+my $request = POST($url, Content_Type => 'application/json', Content => encode_json({}));
+
+sub entry_page_builds {
     my ($cb, $app, $obj, $org_obj) = @_;
-    my $plugin = MT->component('NetlifyBuildHooks');
     require MT::Entry;
     return unless $obj->status == MT::Entry::RELEASE();
-
-    my $app_id = $plugin->get_config_value('netlify_build_hooks', 'system');
     return 1 unless $app_id;
-
-    my $url = NetlifyBuildHooksUrl . $app_id; #webhook url
-    my $request = POST($url, Content_Type => 'application/json', Content => encode_json({}));
-
-    my $ua = LWP::UserAgent->new;
-
-    $obj->save
-        or die $obj->errstr;
-    $ua->request($request);
-}
-
-sub entry_delete_builds {
-    my ($cb, $app, $obj, $org_obj) = @_;
-    my $plugin = MT->component('NetlifyBuildHooks');
-
-    my $app_id = $plugin->get_config_value('netlify_build_hooks', 'system');
-    return 1 unless $app_id;
-
-    my $url = NetlifyBuildHooksUrl . $app_id; #webhook url
-    my $request = POST($url, Content_Type => 'application/json', Content => encode_json({}));
-
-    my $ua = LWP::UserAgent->new;
-    $ua->request($request);
-}
-
-sub page_builds {
-    my ($cb, $app, $obj, $org_obj) = @_;
-    my $plugin = MT->component('NetlifyBuildHooks');
-    require MT::Page;
-    return unless $obj->status == MT::Page::RELEASE();
-
-    my $app_id = $plugin->get_config_value('netlify_build_hooks', 'system');
-    return 1 unless $app_id;
-
-    my $url = NetlifyBuildHooksUrl . $app_id; #webhook url
-    my $request = POST($url, Content_Type => 'application/json', Content => encode_json({}));
 
     my $ua = LWP::UserAgent->new;
 
@@ -60,21 +26,21 @@ sub page_builds {
 
 sub content_data_builds {
     my ($cb, $app, $obj, $org_obj) = @_;
-    my $plugin = MT->component('NetlifyBuildHooks');
     require MT::ContentStatus;
     return unless $obj->status == MT::ContentStatus::RELEASE();
-
-    my $app_id = $plugin->get_config_value('netlify_build_hooks', 'system');
     return 1 unless $app_id;
-
-    my $url = NetlifyBuildHooksUrl . $app_id; #webhook url
-    my $request = POST($url, Content_Type => 'application/json', Content => encode_json({}));
 
     my $ua = LWP::UserAgent->new;
 
     $ua->request($request);
     $obj->save
         or die $obj->errstr;
+}
+
+# delete処理
+sub delete_builds {
+    my $ua = LWP::UserAgent->new;
+    $ua->request($request);
 }
 
 1;
