3b34b6c9c47e9f290e760694ee152b57969ebdd3
redamoon <yuuki60824@gmail.com>
2020-11-06 17:12:44 +0900

    fix

--- plugins/NetlifyBuildHooks/lib/NetlifyBuildHooks/Callbacks.pm
+++ plugins/NetlifyBuildHooks/lib/NetlifyBuildHooks/Callbacks.pm
@@ -1,47 +1,50 @@
 package NetlifyBuildHooks::Callbacks;
 use strict;
 
+use MT;
+use MT::EntryStatus qw(:all);
+
 use constant NetlifyBuildHooksUrl => 'https://api.netlify.com/build_hooks/';
-use LWP::UserAgent;
 use HTTP::Request::Common;
 use JSON;
 
-my $plugin = MT->component('NetlifyBuildHooks');
-my $app_id = $plugin->get_config_value('netlify_build_hooks', 'system');
-my $url = NetlifyBuildHooksUrl . $app_id; #webhook url
-my $request = POST($url, Content_Type => 'application/json', Content => encode_json({}));
+sub request {
+    require MT::Util::Log;
+    MT::Util::Log->init();
+    my $plugin = MT->component('NetlifyBuildHooks');
+    my $app_id = $plugin->get_config_value('netlify_build_hooks', 'system');
+    MT::Util::Log->info('NetlifyBuildHooks: no app_id') unless $app_id;
+    return unless $app_id;
 
-sub entry_page_builds {
-    my ($cb, $app, $obj, $org_obj) = @_;
-    require MT::Entry;
-    return unless $obj->status == MT::Entry::RELEASE();
-    return 1 unless $app_id;
-
-    my $ua = LWP::UserAgent->new;
-
-    $obj->save
-        or die $obj->errstr;
-    $ua->request($request);
+    my $url = NetlifyBuildHooksUrl . $app_id; #webhook url
+    my $ua = MT->new_ua( { timeout => 10 } );
+    return unless $ua;
+    my $request = POST($url, Content_Type => 'application/json', Content => encode_json({}));
+    my $response = $ua->request($request);
+    return unless $response->is_success();
+    return 1;
 }
 
-sub content_data_builds {
+sub post_save_builds {
     my ($cb, $app, $obj, $org_obj) = @_;
-    require MT::ContentStatus;
-    return unless $obj->status == MT::ContentStatus::RELEASE();
-    return 1 unless $app_id;
-
-    my $ua = LWP::UserAgent->new;
-
-    $ua->request($request);
-    $obj->save
-        or die $obj->errstr;
+    return 1 unless $org_obj->status == MT::EntryStatus::HOLD() && $org_obj->status == MT::EntryStatus::RELEASE();
+    require MT::Util::Log;
+    MT::Util::Log->init();
+    MT::Util::Log->info('NetlifyBuildHooks: post_save:obj:' . ref($obj) . ', status=' . status_text($obj->status));
+    MT::Util::Log->info('NetlifyBuildHooks: post_save:org:' . ref($org_obj) . ', status=' . status_text($org_obj->status)) if $org_obj;
+    return 1 unless request();
+    1;
 }
 
 # delete処理
 sub delete_builds {
+    my ($cb, $app, $obj) = @_;
     my $ua = LWP::UserAgent->new;
-    $ua->request($request);
+    require MT::Util::Log;
+    MT::Util::Log->init();
+    MT::Util::Log->info('NetlifyBuildHooks: post_delete:' . ref($obj));
+    return 1 unless request();
+    1;
 }
 
 1;
-
