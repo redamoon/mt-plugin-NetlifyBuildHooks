0b26d07ed90a1a5158197c53be5ec4617e39b62e
redamoon <yuuki60824@gmail.com>
2020-11-25 09:43:25 +0900

    add 画面追加

--- plugins/NetlifyBuildHooks/lib/NetlifyBuildHooks/CMS.pm
+++ plugins/NetlifyBuildHooks/lib/NetlifyBuildHooks/CMS.pm
@@ -8,12 +8,14 @@ use constant NetlifyBuildHooksUrl => 'https://api.netlify.com/build_hooks/';
 use HTTP::Request::Common;
 use JSON;
 
+sub instance { MT->instance->component('NetlifyBuildHooks'); }
+
 sub request {
     my ($id) = @_;
     require MT::Util::Log;
     MT::Util::Log->init();
     my $plugin = MT->component('NetlifyBuildHooks');
-    my $app_id = $plugin->get_config_value($id, 'system');
+    my $app_id = $plugin->get_config_value($id, 'blog');
     MT::Util::Log->info('NetlifyBuildHooks: no app_id') unless $app_id;
     return unless $app_id;
 
@@ -31,7 +33,37 @@ sub production_build {
 }
 
 sub develop_build {
-    return 1 unless request('netlify_build_hooks_develop');
+    request('netlify_build_hooks_develop');
+    return instance->load_tmpl('netlify_build.tmpl');
+}
+
+sub netlify {
+    my $app = shift;
+    my $plugin = MT->component('NetlifyBuildHooks');
+    my $production_id =  $plugin->get_config_value('netlify_build_hooks_production', 'blog');
+    my $develop_id = $plugin->get_config_value('netlify_build_hooks_develop', 'blog');
+    my $params = {
+        production => $production_id,
+        develop   => $develop_id
+    };
+    $app->build_menus( $params );
+    return instance->load_tmpl('netlify_build.tmpl', $params);
+}
+
+sub save_production {
+    my $app = shift;
+    my %param;
+    my $q = $app->param;
+    my $id = $q->param('blog_id');
+    if (!$id) {
+        $param{ new_object } = 1;
+        $param{ page_title } = MT->translate('Netlify Build Hooks');
+    } else {
+
+    }
+    $param{ saved } = $q->param('saved');
+    my $tmpl = 'netlify_build.tmpl';
+    return $app->build_page($tmpl, \%param);
 }
 
 1
--- plugins/NetlifyBuildHooks/lib/NetlifyBuildHooks/L10N/ja.pm
+++ plugins/NetlifyBuildHooks/lib/NetlifyBuildHooks/L10N/ja.pm
@@ -5,13 +5,16 @@ use base 'NetlifyBuildHooks::L10N::en_us';
 use vars qw( %Lexicon );
 
 %Lexicon = (
+    'Production'                                                          => '本番',
+    'Develop'                                                             => '開発',
     'Production ID'                                                       => '本番環境 ID',
     'Develop ID'                                                          => '開発環境 ID',
     'Production build'                                                    => '本番ビルド',
     'Develop build'                                                       => '開発ビルド',
+    'Build'                                                               => 'ビルドする',
     'Enter your Netlify Build Hooks ID for your development environment.' => '開発環境のNetlify Build Hooks IDを入力してください。',
     'Enter your production Netlify Build Hooks ID.'                       => '本番環境のNetlify Build Hooks IDを入力してください。',
-    'Netlify Build Hooks Plugin'                                          => 'NetlifyBuildHooks',
+    'Netlify Build Hooks Plugin'                                          => 'Netlify Build Hooks',
     '_PLUGIN_DESCRIPTION'                                                 => 'Netlify Build Hooksを実行する',
     '_PLUGIN_AUTHOR'                                                      => 'redamoon',
 );
