f510574d20874fde097d5515211b1091e58677c4
redamoon <yuuki60824@gmail.com>
2020-11-26 15:32:39 +0900

    fix

--- plugins/NetlifyBuildHooks/lib/NetlifyBuildHooks/CMS.pm
+++ plugins/NetlifyBuildHooks/lib/NetlifyBuildHooks/CMS.pm
@@ -7,18 +7,25 @@ use constant NetlifyBuildHooksUrl => 'https://api.netlify.com/build_hooks/';
 use HTTP::Request::Common;
 use JSON;
 
+sub plugin {
+    return MT->component('NetlifyBuildHooks');
+}
 sub instance { MT->instance->component('NetlifyBuildHooks'); }
+sub get_blog_id {
+    my $app = shift;
+}
 
 sub request {
-    my ($id) = @_;
+    my ($key, $blog_id) = @_;
     require MT::Util::Log;
     MT::Util::Log->init();
-    my $plugin = MT->component('NetlifyBuildHooks');
-    my $app_id = $plugin->get_config_value($id);
+    my $plugin = plugin();
+    my $app_id = $plugin->get_config_value($key, 'blog:' . $blog_id);
     MT::Util::Log->info('NetlifyBuildHooks: no app_id') unless $app_id;
     return unless $app_id;
 
-    my $url = NetlifyBuildHooksUrl . $app_id; #webhook url
+    # webhook url
+    my $url = NetlifyBuildHooksUrl . $app_id;
     my $ua = MT->new_ua( { timeout => 10 } );
     return unless $ua;
     my $request = POST($url, Content_Type => 'application/json', Content => encode_json({}));
@@ -38,16 +45,22 @@ sub setting {
 # 本番サイトを保存する
 sub production {
     my $app = shift;
+    my $blog = $app->blog;
+    my $blog_id;
+    $blog_id = $blog->id;
     $app->add_return_arg(save_production => 1);
-    request('netlify_build_hooks_production');
+    request('netlify_build_hooks_production', $blog_id);
     $app->call_return;
 }
 
 # 開発サイトを保存する
 sub develop {
     my $app = shift;
+    my $blog = $app->blog;
+    my $blog_id;
+    $blog_id = $blog->id;
     $app->add_return_arg(save_develop => 1);
-    request('netlify_build_hooks_develop');
+    request('netlify_build_hooks_develop', $blog_id);
     $app->call_return;
 }
 
