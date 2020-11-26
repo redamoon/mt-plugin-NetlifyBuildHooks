3c0f69566eb7b05f5bafdfea36efd95985ad15c4
redamoon <yuuki60824@gmail.com>
2020-11-26 14:36:57 +0900

    fix

--- plugins/NetlifyBuildHooks/lib/NetlifyBuildHooks/L10N/ja.pm
+++ plugins/NetlifyBuildHooks/lib/NetlifyBuildHooks/L10N/ja.pm
@@ -12,6 +12,8 @@ use vars qw( %Lexicon );
     'Production build'                                                    => '本番ビルド',
     'Develop build'                                                       => '開発ビルド',
     'Build'                                                               => 'ビルドする',
+    'I ran a development build of Netlify.'                               => 'Netlifyの開発ビルドを実行しました。',
+    'I ran a production build of Netlify.'                                => 'Netlifyの本番ビルドを実行しました。',
     'Enter your Netlify Build Hooks ID for your development environment.' => '開発環境のNetlify Build Hooks IDを入力してください。',
     'Enter your production Netlify Build Hooks ID.'                       => '本番環境のNetlify Build Hooks IDを入力してください。',
     'Netlify Build Hooks Plugin'                                          => 'Netlify Build Hooks',
