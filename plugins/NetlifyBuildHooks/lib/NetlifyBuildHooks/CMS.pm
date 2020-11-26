8d6d096007bcaa7e5454d1cf5e852307ebebe587
redamoon <yuuki60824@gmail.com>
2020-11-26 13:18:50 +0900

    fixリファクタ

--- plugins/NetlifyBuildHooks/lib/NetlifyBuildHooks/CMS.pm
+++ plugins/NetlifyBuildHooks/lib/NetlifyBuildHooks/CMS.pm
@@ -2,7 +2,6 @@ package NetlifyBuildHooks::CMS;
 use strict;
 
 use MT;
-use MT::EntryStatus qw(:all);
 
 use constant NetlifyBuildHooksUrl => 'https://api.netlify.com/build_hooks/';
 use HTTP::Request::Common;
@@ -15,7 +14,7 @@ sub request {
     require MT::Util::Log;
     MT::Util::Log->init();
     my $plugin = MT->component('NetlifyBuildHooks');
-    my $app_id = $plugin->get_config_value($id, 'blog');
+    my $app_id = $plugin->get_config_value($id);
     MT::Util::Log->info('NetlifyBuildHooks: no app_id') unless $app_id;
     return unless $app_id;
 
@@ -25,45 +24,31 @@ sub request {
     my $request = POST($url, Content_Type => 'application/json', Content => encode_json({}));
     my $response = $ua->request($request);
     return unless $response->is_success();
-    return $id;
+    return 1;
 }
 
-sub production_build {
-    return 1 unless request('netlify_build_hooks_production');
-}
-
-sub develop_build {
-    request('netlify_build_hooks_develop');
+# 設定一覧
+sub setting {
+    my $app = shift;
+    my $plugin = MT->component('NetlifyBuildHooks');
+    $app->add_breadcrumb($plugin->translate('Netlify Build Hooks Plugin'));
     return instance->load_tmpl('netlify_build.tmpl');
 }
 
-sub netlify {
+# 本番サイトを保存する
+sub production {
     my $app = shift;
-    my $plugin = MT->component('NetlifyBuildHooks');
-    my $production_id =  $plugin->get_config_value('netlify_build_hooks_production', 'blog');
-    my $develop_id = $plugin->get_config_value('netlify_build_hooks_develop', 'blog');
-    my $params = {
-        production => $production_id,
-        develop   => $develop_id
-    };
-    $app->build_menus( $params );
-    return instance->load_tmpl('netlify_build.tmpl', $params);
+    $app->add_return_arg(save_production => 1);
+    request('netlify_build_hooks_production');
+    $app->call_return;
 }
 
-sub save_production {
+# 開発サイトを保存する
+sub develop {
     my $app = shift;
-    my %param;
-    my $q = $app->param;
-    my $id = $q->param('blog_id');
-    if (!$id) {
-        $param{ new_object } = 1;
-        $param{ page_title } = MT->translate('Netlify Build Hooks');
-    } else {
-
-    }
-    $param{ saved } = $q->param('saved');
-    my $tmpl = 'netlify_build.tmpl';
-    return $app->build_page($tmpl, \%param);
+    $app->add_return_arg(save_develop => 1);
+    request('netlify_build_hooks_develop');
+    $app->call_return;
 }
 
 1
--- plugins/NetlifyBuildHooks/lib/NetlifyBuildHooks/Callbacks.pm
+++ plugins/NetlifyBuildHooks/lib/NetlifyBuildHooks/Callbacks.pm
@@ -9,21 +9,27 @@ use HTTP::Request::Common;
 use JSON;
 
 sub request {
-    my ($id) = @_;
-    require MT::Util::Log;
-    MT::Util::Log->init();
-    my $plugin = MT->component('NetlifyBuildHooks');
-    my $app_id = $plugin->get_config_value('netlify_build_hooks_develop', 'system');
-    MT::Util::Log->info('NetlifyBuildHooks: no app_id') unless $app_id;
-    return unless $app_id;
-
-    my $url = NetlifyBuildHooksUrl . $app_id; #webhook url
-    my $ua = MT->new_ua( { timeout => 10 } );
-    return unless $ua;
-    my $request = POST($url, Content_Type => 'application/json', Content => encode_json({}));
-    my $response = $ua->request($request);
-    return unless $response->is_success();
-    return 2;
+    my $app = shift;
+    my $blog_id = $app->param('blog_id') || 0;
+    return $app->redirect($app->uri(
+        mode => 'netlify_build',
+        args => {blog_id => $blog_id}
+    ));
+    # my ($id) = @_;
+    # require MT::Util::Log;
+    # MT::Util::Log->init();
+    # my $plugin = MT->component('NetlifyBuildHooks');
+    # my $app_id = $plugin->get_config_value('netlify_build_hooks_production', 'blog');
+    # MT::Util::Log->info('NetlifyBuildHooks: no app_id') unless $app_id;
+    # return unless $app_id;
+    #
+    # my $url = NetlifyBuildHooksUrl . $app_id; #webhook url
+    # my $ua = MT->new_ua( { timeout => 10 } );
+    # return unless $ua;
+    # my $request = POST($url, Content_Type => 'application/json', Content => encode_json({}));
+    # my $response = $ua->request($request);
+    # return unless $response->is_success();
+    # return 1;
 }
 
 sub production_build {
@@ -31,7 +37,28 @@ sub production_build {
 }
 
 sub develop_build {
-    return 1 unless request();
+    return 1 unless request('netlify_build_hooks_develop');
 }
 
-1
+sub post_save_builds {
+    my ($cb, $app, $obj, $org_obj) = @_;
+    # return 1 unless $org_obj && $org_obj->status == MT::EntryStatus::RELEASE();
+    # require MT::Util::Log;
+    # MT::Util::Log->init();
+    # MT::Util::Log->info('NetlifyBuildHooks: post_save:obj:' . ref($obj) . ', status=' . status_text($obj->status));
+    # MT::Util::Log->info('NetlifyBuildHooks: post_save:org:' . ref($org_obj) . ', status=' . status_text($org_obj->status)) if $org_obj;
+    return request();
+}
+
+# delete処理
+sub delete_builds {
+    my ($cb, $app, $obj) = @_;
+    my $ua = LWP::UserAgent->new;
+    require MT::Util::Log;
+    MT::Util::Log->init();
+    MT::Util::Log->info('NetlifyBuildHooks: post_delete:' . ref($obj));
+    return 1 unless request();
+    1;
+}
+
+1;
